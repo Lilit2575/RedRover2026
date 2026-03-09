9fdc3295f69c777b906dd830dd97ba23d2d4e441
Lilit2575 <lilo-75@mail.ru>
2026-03-09 12:36:02 +0300

    HW_1

---
+++ JAVA/src/HomWork_1/Main1_hw1.java
@@ -0,0 +1,8 @@
+public class Main1_hw1 {
+    static void main() {
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Поехали!!!");
+    }
+}
---
+++ JAVA/src/HomWork_1/Main1_hw3.java
@@ -0,0 +1,5 @@
+public class Main1_hw3 {
+    static void main() {
+        System.out.println("Смайлик \uD83D\uDE00");
+    }
+}
